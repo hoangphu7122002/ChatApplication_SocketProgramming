1b1f5be181e3c22feb9e15ed3039c012ed852e34
hoangphu7122002 <phu.nguyen7122002hp@hcmut.edu.vn>
2022-11-26 22:35:39 +0700

    refactor2

--- assignment/client.py
+++ assignment/client.py
@@ -26,57 +26,6 @@ ours_server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 ours_server.bind(('',our_port))
 ours_server.listen(QUEUE_CLIENT)
 
-def get_peer_element(peer_list, my_peer_id):
-    for peer in peer_list:
-        if peer[3] == my_peer_id:
-            return peer    
-
-def is_already_Connected(active_conn, id_peer):    
-    for conn in active_conn:
-        if conn[3] is id_peer:
-            return True
-    return False
-    
-def get_sockpeer_element(active_conn_sock, id_to_find):
-    for conn in active_conn_sock:
-        if conn[0][3] is id_to_find:
-            return conn[1]    
-        
-def getPeerId(msg):
-    try:
-        return int(msg.split(' ')[1])
-    except:
-        return 0
-
-def get_msg_to_send(msg):
-    text_l = msg.split(' ')
-    message = ''
-    for i in range(2,len(text_l)):
-        message = message + text_l[i]
-        if i != len(text_l) - 1:
-            message = message + ' '
-    return message
-
-def send_client_message(obj_message):
-    msg = pickle.dumps(obj_message)
-    msg = bytes(f"{len(msg):<{HEADER_LENGTH}}","utf-8") + msg
-    return msg   
-    
-def get_client_data(server):
-    header_length = server.recv(HEADER_LENGTH)
-    message_length = int(header_length.decode("utf-8").strip())
-    data_res = server.recv(message_length)
-    data_res = pickle.loads(data_res)
-    
-    return data_res
-    
-def is_command(msg, str_cmd):
-    return msg.count(str_cmd)    
-    
-#Handler CTRL+C - Close connection with server
-def signal_handler(sig, frame):
-    pass
-
 def connect_server():
     password = input('>password:')
     message = {}
--- assignment/helper_function.py
+++ assignment/helper_function.py
@@ -1,3 +1,6 @@
+import pickle
+HEADER_LENGTH = 10
+
 def print_help(name):
     print('Hi '+name+' !\n\n')
     print('These are the commands that you can use:\n')
@@ -27,3 +30,54 @@ def print_conn_table(name, active_conn):
     for peer in active_conn:
         print('+ Name: '+peer[0]+'\t | Port: '+str(peer[1])+' | Ip: '+peer[2]+' | Id_peer: '+str(peer[3]))
     input("\n\n\nPress Enter to continue...")
+    
+def get_peer_element(peer_list, my_peer_id):
+    for peer in peer_list:
+        if peer[3] == my_peer_id:
+            return peer    
+
+def is_already_Connected(active_conn, id_peer):    
+    for conn in active_conn:
+        if conn[3] is id_peer:
+            return True
+    return False
+    
+def get_sockpeer_element(active_conn_sock, id_to_find):
+    for conn in active_conn_sock:
+        if conn[0][3] is id_to_find:
+            return conn[1]    
+        
+def getPeerId(msg):
+    try:
+        return int(msg.split(' ')[1])
+    except:
+        return 0
+
+def get_msg_to_send(msg):
+    text_l = msg.split(' ')
+    message = ''
+    for i in range(2,len(text_l)):
+        message = message + text_l[i]
+        if i != len(text_l) - 1:
+            message = message + ' '
+    return message
+
+def send_client_message(obj_message):
+    msg = pickle.dumps(obj_message)
+    msg = bytes(f"{len(msg):<{HEADER_LENGTH}}","utf-8") + msg
+    return msg   
+    
+def get_client_data(server):
+    header_length = server.recv(HEADER_LENGTH)
+    message_length = int(header_length.decode("utf-8").strip())
+    data_res = server.recv(message_length)
+    data_res = pickle.loads(data_res)
+    
+    return data_res
+    
+def is_command(msg, str_cmd):
+    return msg.count(str_cmd)    
+    
+#Handler CTRL+C - Close connection with server
+def signal_handler(sig, frame):
+    pass
